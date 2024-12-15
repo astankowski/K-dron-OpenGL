c5ccef7ecd3d4ec71be279420d0e290d44767bf4
s27549 <s27549@pjwstk.edu.pl>
2024-12-15 11:43:03 +0100

    Initial

---
+++ kdron.h
@@ -0,0 +1,28 @@
+#ifndef KDRON_H
+#define KDRON_H
+
+#include <GL/glew.h>
+
+#include "indexmodel.h"
+#include "movablemodel.h"
+#include "modelprogram.h"
+#include "matma.h"
+
+class KDRON: public IndexModel, public MovableModel{
+ public:
+    Cube(float init_velocity=10, float init_angle=0);
+    void SetInitAngle(float angle){angle_= angle;}
+    void SetVelocity(float velocity){velocity_= velocity;}
+    void Initialize();
+    void Draw(const ModelProgram & program) const;
+    void Move(float delta_t);
+    void SpeedUp();
+    void SlowDown();
+    void ToggleAnimated();
+ private:
+    float angle_;
+    float velocity_;
+    bool animated_;
+};
+
+#endif // KDRON_H
